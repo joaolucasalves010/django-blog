29a9d014ca077db2681eaee9b567b7bfa95cb1e3
João Lucas <ylucasslalves@gmail.com>
2025-02-21 18:17:58 -0300

    criando model Post

--- django_blog_app/blog/models.py
+++ django_blog_app/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+  class Meta:
+    verbose_name = 'Post'
+    verbose_name_plural = 'Posts'
+
+  title = models.CharField(max_length=200)
+  content = models.TextField()
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  favicon = models.ImageField(upload_to='posts/favicon', default=None, blank=True)
+
+  def __str__(self):
+    return self.title
