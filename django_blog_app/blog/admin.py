f753cd0b9f580914990300b3b3f3cfa5e1a96703
João Lucas <ylucasslalves@gmail.com>
2025-02-21 18:17:28 -0300

    criando model PostAdmin

--- django_blog_app/blog/admin.py
+++ django_blog_app/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from blog import models
 
 # Register your models here.
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = 'id', 'title', 'author',
+  search_fields = 'id', 'title', 'author',
