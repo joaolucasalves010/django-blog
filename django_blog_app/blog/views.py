b1626c8e5f9f81ff1b7aad4e0e664f7c74920926
João Lucas <ylucasslalves@gmail.com>
2025-02-24 20:54:07 -0300

    adicionando paginator no index

--- django_blog_app/blog/views.py
+++ django_blog_app/blog/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
 
   posts = Post.objects.all()
-
-  return render(request, 'blog/pages/index.html', {'posts': posts,})
+  paginator = Paginator(posts, 2)
+  
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number)
+  return render(request, 'blog/pages/index.html', {'page_obj': page_obj,})
 
 def post(request, post_title, id):
   post = get_object_or_404(Post, id=id)
