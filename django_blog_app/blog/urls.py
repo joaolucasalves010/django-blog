f3d986a8af3ffe3f0c7b9a003f99d2d84cfdf5dd
João Lucas <ylucasslalves@gmail.com>
2025-03-09 08:56:08 -0300

    criando urls de register, login e logout, e mudando url de post

--- django_blog_app/blog/urls.py
+++ django_blog_app/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from blog.views import index, post
+from blog.views import index, post, register, login_view, logout_view
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('post/<str:post_title>/<int:id>', post, name="post"),
+    path('post/<str:post_author>/<int:id>', post, name="post"),
+    path('register', register, name="register"),
+    path('login', login_view, name='login'),
+    path('logout', logout_view, name='logout'),
 ]
