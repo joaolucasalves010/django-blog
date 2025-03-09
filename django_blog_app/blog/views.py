41d70abcb1830a9f4657a01664f6ca0a78518cd3
João Lucas <ylucasslalves@gmail.com>
2025-03-09 08:57:40 -0300

    decoradores de login_required e views de register, login e logout

--- django_blog_app/blog/views.py
+++ django_blog_app/blog/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Post
 from django.core.paginator import Paginator
+from blog.forms import CustomRegisterForm, CustomLoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='blog:login')
 def index(request):
-
   posts = Post.objects.all().order_by('-id')
   paginator = Paginator(posts, 5)
   
@@ -12,7 +15,8 @@ def index(request):
   page_obj = paginator.get_page(page_number)
   return render(request, 'blog/pages/index.html', {'page_obj': page_obj,})
 
-def post(request, post_title, id):
+@login_required(login_url='blog:login')
+def post(request, post_author, id):
   post = get_object_or_404(Post, id=id)
 
   context = {
@@ -20,3 +24,36 @@ def post(request, post_title, id):
   }
 
   return render(request, 'blog/pages/post.html', context)
+
+def register(request):
+  form = CustomRegisterForm()
+  if request.user.is_authenticated:
+    return redirect('blog:index')
+
+  if request.method == "POST":
+    form = CustomRegisterForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+      user.save()
+      return redirect('blog:login')
+
+  return render(request, 'blog/pages/user_register.html', {'form': form})
+
+def login_view(request):
+  form = CustomLoginForm()
+  if request.user.is_authenticated:
+    return redirect('blog:index')
+
+  if request.method == "POST":
+    form = CustomLoginForm(data=request.POST)
+    if form.is_valid():
+      user = form.get_user()
+      login(request, user)
+      return redirect('blog:index')
+    
+  return render(request, 'blog/pages/user_login.html', {'form': form})
+
+
+def logout_view(request):
+  logout(request)
+  return redirect('blog:login')
